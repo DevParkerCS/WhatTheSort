bad75c02c645282b871ae1b34b57d7c2dfd43d02
Parker <parkerphillips2003@gmail.com>
2023-10-03 20:49:32 -0700

    added Insertion Sort algorithm

---
+++ src/Utils/SortAlgos.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+export const bubbleSort = async (
+  divsToSort: JSX.Element[],
+  setDivsToSort: React.Dispatch<React.SetStateAction<JSX.Element[]>>,
+  setIsSorting: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsSorted: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  setIsSorting(true);
+
+  for (let i = 0; i < divsToSort.length - 1; i++) {
+    for (let j = 0; j < divsToSort.length - i - 1; j++) {
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve(true);
+        }, 1)
+      );
+      if (divsToSort[j].props.height > divsToSort[j + 1].props.height) {
+        let temp = divsToSort[j];
+        divsToSort[j] = divsToSort[j + 1];
+        divsToSort[j + 1] = temp;
+        let newArr = divsToSort.slice();
+        setDivsToSort(newArr);
+      }
+    }
+  }
+  setIsSorting(false);
+  setIsSorted(true);
+};
+
+export const insertionSort = async (
+  divsToSort: JSX.Element[],
+  setDivsToSort: React.Dispatch<React.SetStateAction<JSX.Element[]>>,
+  setIsSorting: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsSorted: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  setIsSorting(true);
+
+  for (let i = 0; i < divsToSort.length; i++) {
+    let keyVal = divsToSort[i];
+    let j = i - 1;
+
+    while (j >= 0 && keyVal.props.height < divsToSort[j].props.height) {
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve(true);
+        }, 5)
+      );
+      divsToSort[j + 1] = divsToSort[j];
+      j = j - 1;
+      let newArr = divsToSort.slice();
+      setDivsToSort(newArr);
+    }
+    divsToSort[j + 1] = keyVal;
+    setDivsToSort(divsToSort.slice());
+  }
+  setIsSorting(false);
+  setIsSorted(true);
+};
